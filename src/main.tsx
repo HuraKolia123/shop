e282ba08cff98fa866e724d3ca502e11004208e4
HuraKolia123 <kolyagura569@ukr.net>
2024-08-28 19:27:38 +0300

    add import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./app/App.tsx";
 import "./index.scss";
-// import { StoreProvider } from "./app/providers/StoreProvider.tsx";
+import { StoreProvider } from "./app/providers/StoreProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
